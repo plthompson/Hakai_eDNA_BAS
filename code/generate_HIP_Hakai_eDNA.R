4915037633e4bcff06ce4edaaf3d2dbe117bdfbd
Thompson <Patrick.Thompson@dfo-mpo.gc.ca>
2021-04-20 14:00:48 -0700

    script that performs Halton iterative partitioning

---
+++ code/generate_HIP_Hakai_eDNA.R
@@ -0,0 +1,67 @@
+# code by Patrick Thompson
+# April 2021
+# patrick.thompson@dfo-mpo.gc.ca
+
+#load packages#####
+library(BASMasterSample)
+library(rgdal)
+library(sf)
+library(tidyverse)
+library(data.table)
+library(raster)
+
+#change plot theme####
+source("./code/functions/plot_theme.R")
+theme_set(plt_theme)
+
+#load research area####
+research_area <- readOGR("./spatial_data/calvert_research_area.gpkg")
+research_area <- st_as_sf(research_area, wtk = geometry)
+area_plot <- ggplot()+
+  geom_sf(data = research_area, fill = NA)
+area_plot
+
+#read in habitat lines
+habitat_line_features <- readOGR("./spatial_data/habitat_line_features.gpkg")
+habitat_line_features <- st_as_sf(habitat_line_features)
+
+#create Halton boxes#####
+# note - bounding box should be all of BC to make boxes align with greater BC BAS
+# however this is slow and so for now we will use the study region as the bounding box
+#make bounding box
+bb <- buildMS(research_area, d = 2, FALSE)
+
+boxes <- 50 #choose number of sites to include
+box_size <- 2000 #chose size of halton box
+
+halton_boxes <- point2Frame(pts = habitat_line_features, bb = bb, size = box_size)
+
+area_plot+
+  geom_sf(data = habitat_line_features, size = 0.3) +
+  geom_sf(data = halton_boxes, fill = NA, size = 0.3, color = 1)
+
+#perform Halton Iterative Partitioning####
+#library(devtools)
+#install_github("paul-vdb/NWTMasterSample", build_vignettes = TRUE)
+source('./code/functions/HSS.r')
+
+coords <- st_coordinates(st_centroid(halton_boxes))
+
+bbox <- matrix(st_bbox(bb), nrow = 2, ncol = 2)
+
+HSS.pts <- getHipSample(X = coords[,1], Y = coords[,2], index = halton_boxes$HaltonIndex,
+                        N = 27, bb = bbox,  base = c(2,3), quiet = TRUE,
+                        Ps1 = 0:1, Ps2 = 0:2, hipS1 = 0:1, hipS2 = 0:2)
+
+# Chooses a sample from all boxes. Small wrapper function.
+n <- 20
+pts.new <- getSamples(HSS.pts, n = n)
+HIPBoxes <- st_as_sf(getHIPBoxes(hip = HSS.pts, bb = bbox, n = n, base = c(2,3)))
+HIPBoxes <- st_set_crs(HIPBoxes, value = st_crs(halton_boxes)) #something is wrong here since the HIP boxes don't line up properly
+
+halton_boxes_select <- halton_boxes %>% filter(HaltonIndex %in% pts.new$index)
+
+area_plot+
+  geom_sf(data = habitat_line_features, size = 0.3) +
+  geom_sf(data = HIPBoxes, fill = NA)+
+  geom_sf(data = halton_boxes_select, fill = NA, size = 1, color = 2)
